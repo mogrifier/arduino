fd77bb845761d53afabaaf9e6817a87b4bcf6fcb
Erich Izdepski <mogrifier@gmail.com>
2022-12-08 10:20:18 -0500

    Refeactored code using static variables and parameters by reference.

--- fractalmusic/fractalmusic.ino
+++ fractalmusic/fractalmusic.ino
@@ -8,32 +8,19 @@ This is a continuous player- run through FX for sure!
 duration should be less than loopdelay
 
 */
+//#define DEBUG true
 
 const int SPEAKERPIN = 9;
 const int MODPIN = 0;
 const int FREQ = 500;
 const int LOOPDELAY = 15;
 const int DURATION = 12;  //you can hear down to 11ms or so. combined with LOOPDELAY this creates extra sync sound
-const int MAXMOD = 10;
 const float TWELFTHROOT = 1.05946;
 const float LOWA = 27.5;
 
-int count = 0;
-int deltaFreq = -5;
-int inc = 3;
-int totalIterations = 0;
-
-//array for pitch and duration (first value is pitch)
-int music[2];
-//starting point for IFS code. Store past and present for iteration.
-float x = 0;
-float y = 0;
-float next_x = 0;
-float next_y = 0;
-int note_duration = 0;
-int freq = 0;
-//numbers of times a given note is played. cycles * duration >= note_duration
-int cycles = 0;
+//these are not constant since will likely be changeable with a sensor input
+int INC_SENSOR = 3;
+int MAXMOD_SENSOR = 10;
 
 //arrays for holding IFS matrix
 float a[4];
@@ -46,9 +33,6 @@ float f[4];
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
-
-  //random start points
-  init_xy();
 
   //initial IFS matrix data for a fern
   a[0] = 0;
@@ -80,49 +64,54 @@ void setup() {
   f[1] = 1.6;
   f[2] = 1.6;
   f[3] = 0.44;
-
-
 }
 
 void loop() {
 
+  //some variable values need to be saved between calls to loop so declare them as static
+  static int cycles;     //for calculating how long a note has played
+  static int count;      //
+  static int deltaFreq;  //vibrato
+  static int duration;   //how long the note will play (minimum)
+  static int freq;
   // put your main code here, to run repeatedly:
   int sensor = analogRead(MODPIN);
- // int freq = map(sensor, 0, 1023, 100, 3000);  //100 hz is all little 8ohm speaker can get down to??
- 
-  if (cycles * DURATION >= note_duration) {
+
+  if (cycles * DURATION >= duration) {
     //note has played for at least the amount of milliseconds specified so get a new note
     //reset cycle counter
     cycles = 0;
-    compute_music();
-    freq = music[0];
-    note_duration = music[1];
-    char buffer[40];
-    sprintf(buffer, "Pitch %d and duration %d", freq, note_duration);    
-    Serial.println(buffer);    
-  }  
+    //compute new freq and duration values
+    compute_music(freq, duration);
+
+  #ifdef DEBUG {
+      char buffer[40];
+      sprintf(buffer, "Pitch %d and duration %d", freq, duration);
+      Serial.println(buffer);
+  }
+  #endif
+  }
   else {
-    //use previous pitch and duration, but the duration has to "count down"
-    freq = music[0];
-    note_duration = music[1];
+    //use previous freq and duration- values is stored since variables are static, but the duration has to "count down"
     cycles += 1;
   }
 
-  //int mod = map(sensor, 0, 1023, -4, 4);  //+- 2 hz
-  int modRange = map(sensor, 0, 1023, 5, 30);   
+  int modRange = map(sensor, 0, 1023, 5, 30);  //effectively control range and speed of vibrato
   //what should max value be? Need to tie better to freq - make a tunable parameter
   //Serial.print("modrange= ");
   //Serial.println(modRange);
-  //programmable modulation
+
+  //programmable vibrato-like control. This creates a sinusoidal vibrato.
   count++;
-  //this is only causing a slow pulse like a saw- NOT the sync sound. By inverting increment you get a phaser
   if (count % modRange == 0) {
-    deltaFreq += inc;
-    if (deltaFreq >= MAXMOD) {
-      inc = -inc;
+    //reset count (if you don't it would overflow after many hours)
+    count = 0;
+    deltaFreq += INC_SENSOR;
+    if (deltaFreq >= MAXMOD_SENSOR) {
+      INC_SENSOR = -INC_SENSOR;
     }
-    if (deltaFreq <= -MAXMOD) {
-      inc = -inc;
+    if (deltaFreq <= -MAXMOD_SENSOR) {
+      INC_SENSOR = -INC_SENSOR;
     }
   }
 
@@ -135,7 +124,13 @@ void loop() {
 /*
 Get the next pitch and duration from the IFS code. Just compute all as needed.
 */
-void compute_music() {
+void compute_music(int &freq, int &duration) {
+  static int totalIterations;
+  //starting point for IFS code. Store past and present for iteration.
+  static float x;
+  static float y;
+  static float next_x;
+  static float next_y;
 
   totalIterations += 1;
   int k = get_chance();
@@ -149,49 +144,51 @@ void compute_music() {
   //scale values so in bounds and make sense for pitch frequency and duration in milliseconds
   int scale_x = int(abs(x) * 100);
   if (scale_x > 100) {
-    scale_x = 100;    
-  }  
+    scale_x = 100;
+  }
   //constrain the piano key range to one the arduino can play and also not too high since unpleasant
   int piano_key = map(scale_x, 0, 100, 25, 74);
   //y has a range up to 3.5 or so
   int scale_y = int(abs(y) * 600 + 400);
-  
-  music[0] = get_freq(piano_key);
-  music[1] = scale_y;
-  
+
+  //assign values to the variable references so changes are seen in the loop function
+  freq = get_freq(piano_key);
+  duration = scale_y;
+
   if (totalIterations > 100) {
     //reset to new starting point for iteration
-    init_xy();
+    init_xy(x, y);
     totalIterations = 0;
   }
-
 }
 
 /*
 Choose array indices based on a hard-coded probability distribution.
 */
 int get_chance() {
-    float r = (float)random(1, 100)/100;
-    if (r <= 0.1)
-        return 0;
-    if (r <= 0.2)
-        return 1;
-    if ( r <= 0.4)
-        return 2;
-    else
-        return 3;
+  float r = (float)random(1, 100) / 100;
+  if (r <= 0.1)
+    return 0;
+  if (r <= 0.2)
+    return 1;
+  if (r <= 0.4)
+    return 2;
+  else
+    return 3;
 }
 
-void init_xy() {
-  x = (float)random(1, 100)/100;
-  y = (float)random(1, 100)/100;
+void init_xy(float &x, float &y) {
+  x = (float)random(1, 100) / 100;
+  y = (float)random(1, 100) / 100;
 }
+
 /*
 Convert the piano key position (1 to 88) to the corresponding frequency
 */
 int get_freq(int key) {
-  int octave = (int)(key/12);
+  int octave = (int)(key / 12);
   int note = (key % 12) - 1;
   float freq = LOWA * pow(2, octave) * pow(TWELFTHROOT, note);
-  return int(freq);
+  //round to nearest whole value
+  return int(freq + 0.5);
 }
